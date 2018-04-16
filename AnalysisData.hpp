f98045f33cce6a23b77e8dc4b8cb176da404a87a
Krzysztof Gonciarz <gonciarz@mpi-cbg.de>
2018-04-16 12:19:30 +0200

    HDF5 writer fix

--- AnalysisData.hpp
+++ AnalysisData.hpp
@@ -8,6 +8,7 @@
 #include "DataManager.hpp"
 #include "misc/APRTimer.hpp"
 #include "io/hdf5functions_blosc.h"
+#include "data_structures/APR/APR.hpp"
 
 #include <cstdio>
 #include <iostream>
@@ -154,6 +155,178 @@ class AnalysisData: public Data_manager {
     void get_git_version();
 
 };
+
+void hdf5_write_string_blosc(hid_t obj_id,const char* attr_name,std::string output_str){
+    //
+    //  Writes string information as an attribute
+    //
+    //
+
+    hid_t       aid, atype, attr;
+    herr_t      status;
+
+    aid = H5Screate(H5S_SCALAR);
+
+    atype = H5Tcopy (H5T_C_S1);
+
+    if (output_str.size() > 0){
+
+        status = H5Tset_size (atype, output_str.size());
+
+        attr = H5Acreate2(obj_id, attr_name, atype, aid, H5P_DEFAULT,H5P_DEFAULT);
+
+        status = H5Awrite (attr, atype,output_str.c_str());
+    }
+
+}
+
+void write_part_data_to_hdf5(Data_manager& p_rep,hid_t obj_id,std::vector<std::string>& extra_data_type,std::vector<std::string>& extra_data_name,int flag_type,int req_size){
+    //
+    //  Bevan Cheeseman 2016
+    //
+    //  This function writes part_data data types to hdf5 files, according to them satisfying the flag and data length requirements
+    //
+    //  flag_type: allows to set local part_data variables for printing; or deciding which set to prinrt;
+    //
+    //  req_size: allows you to determine if you want only a specific type of data printed out; which is requried if you want a valid xdmf file
+    //
+    //
+
+
+    hsize_t dims;
+    hsize_t rank = 1;
+
+    for(auto const &part_data_info : p_rep.part_data_list) {
+        if (part_data_info.second.print_flag == flag_type) {
+            // add the name
+
+            if (part_data_info.second.data_type=="uint8_t") {
+
+                Part_data<uint8_t>* data_pointer = p_rep.get_data_ref<uint8_t>(part_data_info.first);
+                dims = data_pointer->data.size();
+                //check if it is particle data of cell data
+                if ((data_pointer->data.size() == req_size) | (req_size == 0)) {
+
+                    extra_data_name.push_back(part_data_info.first);
+                    extra_data_type.push_back(part_data_info.second.data_type);
+
+
+
+                    hdf5_write_data_blosc(obj_id,H5T_NATIVE_UINT8,part_data_info.first.c_str(),rank,&dims,data_pointer->data.data(), BLOSC_ZSTD, 1, 2);
+                }
+
+            } else if (part_data_info.second.data_type=="bool"){
+                std::cout << "Bool type can't be stored with xdmf change datatype" << std::endl;
+            } else if (part_data_info.second.data_type=="uint16_t"){
+
+                Part_data<uint16_t>* data_pointer = p_rep.get_data_ref<uint16_t>(part_data_info.first);
+                dims = data_pointer->data.size();
+
+                if ((data_pointer->data.size() == req_size) | (req_size == 0)) {
+                    extra_data_name.push_back(part_data_info.first);
+                    extra_data_type.push_back(part_data_info.second.data_type);
+
+                    hdf5_write_data_blosc(obj_id,H5T_NATIVE_UINT16,part_data_info.first.c_str(),rank,&dims,data_pointer->data.data(), BLOSC_ZSTD, 1, 2 );
+                }
+
+            }else if (part_data_info.second.data_type=="int16_t"){
+
+                Part_data<int16_t>* data_pointer = p_rep.get_data_ref<int16_t>(part_data_info.first);
+                dims = data_pointer->data.size();
+
+                if ((data_pointer->data.size() == req_size) | (req_size == 0)) {
+                    extra_data_name.push_back(part_data_info.first);
+                    extra_data_type.push_back(part_data_info.second.data_type);
+
+                    hdf5_write_data_blosc(obj_id,H5T_NATIVE_INT16,part_data_info.first.c_str(),rank,&dims,data_pointer->data.data(), BLOSC_ZSTD, 1, 2 );
+                }
+
+            }  else if (part_data_info.second.data_type=="int"){
+
+                Part_data<int>* data_pointer = p_rep.get_data_ref<int>(part_data_info.first);
+                dims = data_pointer->data.size();
+
+                if ((data_pointer->data.size() == req_size) | (req_size == 0)) {
+                    extra_data_name.push_back(part_data_info.first);
+                    extra_data_type.push_back(part_data_info.second.data_type);
+
+                    hdf5_write_data_blosc(obj_id,H5T_NATIVE_INT,part_data_info.first.c_str(),rank,&dims,data_pointer->data.data(), BLOSC_ZSTD, 1, 2 );
+
+                }
+            } else if (part_data_info.second.data_type=="float"){
+                Part_data<float>* data_pointer = p_rep.get_data_ref<float>(part_data_info.first);
+                dims = data_pointer->data.size();
+
+                if ((data_pointer->data.size() == req_size) | (req_size == 0)) {
+                    extra_data_name.push_back(part_data_info.first);
+                    extra_data_type.push_back(part_data_info.second.data_type);
+
+                    hdf5_write_data_blosc(obj_id,H5T_NATIVE_FLOAT,part_data_info.first.c_str(),rank,&dims,data_pointer->data.data(), BLOSC_ZSTD, 1, 2 );
+                }
+
+            } else if (part_data_info.second.data_type=="int8_t") {
+
+                Part_data<int8_t>* data_pointer = p_rep.get_data_ref<int8_t>(part_data_info.first);
+                dims = data_pointer->data.size();
+                //check if it is particle data of cell data
+                if ((data_pointer->data.size() == req_size) | (req_size == 0)) {
+
+                    extra_data_name.push_back(part_data_info.first);
+                    extra_data_type.push_back(part_data_info.second.data_type);
+
+
+                    hdf5_write_data_blosc(obj_id,H5T_NATIVE_INT8,part_data_info.first.c_str(),rank,&dims,data_pointer->data.data(), BLOSC_ZSTD, 1, 2  );
+                }
+
+            } else if (part_data_info.second.data_type=="string") {
+                //ONLY WRITES TEH FIRST STRING!!##!$
+
+                Part_data<std::string>* data_pointer = p_rep.get_data_ref<std::string>(part_data_info.first);
+                dims = 1;
+                //check if it is particle data of cell data
+                if ((data_pointer->data.size() == req_size) | (req_size == 0)) {
+
+                    extra_data_name.push_back(part_data_info.first);
+                    extra_data_type.push_back(part_data_info.second.data_type);
+
+                    hdf5_write_string_blosc(obj_id, part_data_info.first.c_str(), data_pointer->data[0]);
+
+                }
+
+            }else {
+                std::cout << "Unknown Type in extra printout encournterd" << std::endl;
+
+            }
+        }
+    }
+
+}
+
+
+inline void AnalysisData::write_analysis_data_hdf5() {
+    std::string save_loc = "";
+    std::string hdf5_file_name = save_loc + file_name + ".h5";
+
+    APRWriter::AprFile f(hdf5_file_name, APRWriter::AprFile::Operation::WRITE);
+
+    //create the main group
+    hid_t pr_groupid = H5Gcreate2(f.fileId, "Analysis_data", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+    H5G_info_t info;
+    H5Gget_info(pr_groupid, &info);
+
+    std::vector<std::string> extra_data_type;
+    std::vector<std::string> extra_data_name;
+
+    int req_size = 0;
+    int flag_type = 1;
+    write_part_data_to_hdf5(*this, pr_groupid, extra_data_type, extra_data_name, flag_type, req_size);
+
+    //close shiz
+    H5Gclose(pr_groupid);
+
+    std::cout << "Data Analysis File Writing Complete" << std::endl;
+}
+
 //inline void AnalysisData::write_analysis_data_hdf5(){
 //
 //    std::string save_loc = "";
